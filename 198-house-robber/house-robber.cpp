4c94f67c343d7fb66c3b22d4163fc868755b8a09
ankita-0906 <161379822+ankita-0906@users.noreply.github.com>
2024-12-07 00:10:16 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (7.91%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -8,10 +8,21 @@ int solve(vector<int>& nums,int index,vector<int>&dp){
        int excl=0+solve(nums,index-1,dp);
        return dp[index]=max(incl,excl);
 }
+int tabsolve(vector<int>&nums,vector<int>&dp){
+    dp[0]=nums[0]; int incl=INT_MIN;
+     for(int i=1;i<nums.size();i++){
+        if(i>1) incl=nums[i]+dp[i-2];
+        else incl=nums[i];
+       int excl=0+dp[i-1];
+        dp[i]=max(incl,excl);
+     }
+     return dp[nums.size()-1];
+}
     int rob(vector<int>& nums) {
         int n =nums.size();
         vector<int>dp(n,-1);
-        int cnt=solve(nums,n-1,dp);
+        //int cnt=solve(nums,n-1,dp);
+        int cnt=tabsolve(nums,dp);
         return cnt;
     }
 };
