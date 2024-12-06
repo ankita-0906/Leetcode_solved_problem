33abbc771c944a6474f913fd940d91e7ca319baf
ankita-0906 <161379822+ankita-0906@users.noreply.github.com>
2024-12-06 17:36:10 +0530

    Time: 53 ms (87.79%) | Memory: 117.5 MB (90.75%) - LeetSync

--- 2640-maximum-number-of-integers-to-choose-from-a-range-i/maximum-number-of-integers-to-choose-from-a-range-i.cpp
+++ 2640-maximum-number-of-integers-to-choose-from-a-range-i/maximum-number-of-integers-to-choose-from-a-range-i.cpp
@@ -1,13 +1,16 @@
 class Solution {
 public:
     int maxCount(vector<int>& arr, int n, int maxsum) {
-        //sort(arr.begin(),arr.end());
-        int sum=0,cnt=0;
+        sort(arr.begin(),arr.end());
+        int sum=0,cnt=0,index=0,len=arr.size();
         for(int i=1;i<=n;i++){
-         if(find(arr.begin(),arr.end(),i)==arr.end() && sum+i<=maxsum){
-            sum=sum+i;cnt++;
+         if(index<len && i==arr[index]){
+            while(index<len && i==arr[index]) index++;
          }
-         
+         else if(sum+i<=maxsum){
+            sum=sum+i; cnt++;
+         }
+         //if(sum>maxsum) break;
         }
         return cnt;
     }
