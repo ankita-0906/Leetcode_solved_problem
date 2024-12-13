763c0b1389d5aa1235e6aaf8fa3978b8d1c95d9a
ankita-0906 <161379822+ankita-0906@users.noreply.github.com>
2024-12-13 17:44:33 +0530

    Time: 31 ms (36.81%) | Memory: 27.5 MB (8.74%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -7,12 +7,28 @@ public:
 
         return dp[id1][id2]=max(solve(s1,s2,id1-1,id2,dp),solve(s1,s2,id1,id2-1,dp));
     }
-
+    int solvetab(string &s1,string &s2,int id1,int id2,vector<vector<int>>&dp){
+        for(int i=0;i<id1;i++){
+            for(int j=0;j<id2;j++){
+                if(s1[i]==s2[j]) {
+                    if(i-1>=0 && j-1>=0)  dp[i][j]=1+dp[i-1][j-1];
+                    else dp[i][j]=1;
+                }
+                else if(s1[i]!=s2[j]){
+                    int left=0,right=0;
+                    if(i-1>=0) left=dp[i-1][j];
+                    if(j-1>=0) right=dp[i][j-1];
+                    dp[i][j]=max(left,right);
+                }
+            }
+        }
+        return dp[id1-1][id2-1];
+    }
     int longestCommonSubsequence(string text1, string text2) {
         int n1=text1.length();
         int n2=text2.length();
         vector<vector<int>>dp(n1,vector<int>(n2,-1));
-        int ans=solve(text1,text2,n1-1,n2-1,dp);
+        int ans=solvetab(text1,text2,n1,n2,dp);
         return ans;
     }
 };
