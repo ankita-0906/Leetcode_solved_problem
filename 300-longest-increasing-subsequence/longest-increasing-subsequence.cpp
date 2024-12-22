71e8c70b1b8b3465b8e0d8140a89fa1f643e34a9
ankita-0906 <161379822+ankita-0906@users.noreply.github.com>
2024-12-22 19:10:06 +0530

    Time: 421 ms (22.96%) | Memory: 576.3 MB (6.50%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -11,10 +11,31 @@ public:
          take =1+solve(nums,index-1,index,dp);
         return dp[index][prev+1]=max(take,not_take); 
     }
+   //----------------------tabulation----------------------------------------
+
+   int solvetab(vector<int>& nums,int n){
+      vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+      int maxi=INT_MIN;
+      for(int index=n-1;index>=0;index--){
+        for(int prev=index-1;prev>=-1;prev--){
+            int non_take=dp[index+1][prev+1];
+            int take=0;
+            if(prev==-1 || nums[index]>nums[prev])
+            take=1+dp[index+1][index+1];
+            dp[index][prev+1]=max(take,non_take) ;
+            maxi=max(maxi,dp[index][prev+1]);
+        }
+      }
+
+      return maxi;
+   }
+    
 
     int lengthOfLIS(vector<int>& nums) {
         int n=nums.size();
         vector<vector<int>>dp(n,vector<int>(n+1,-1));
-        return solve(nums,n-1,-1,dp);
+        //return solve(nums,n-1,-1,dp);
+        return solvetab(nums,n);
+      
     }
 };
