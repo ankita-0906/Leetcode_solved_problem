8c5fecaed475b8a28c830aa2aa86616dd5fd8963
ankita-0906 <161379822+ankita-0906@users.noreply.github.com>
2024-12-24 12:45:57 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (7.79%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
 int solve(int n,vector<int>&dp){
-    if(n==1 || n==0) return 1;
+    if(n==1 ) return 1;
+    if(n==2) return 2;
     if(dp[n]!=-1) return dp[n];
     return dp[n]=solve(n-1,dp)+solve(n-2,dp);
 }
