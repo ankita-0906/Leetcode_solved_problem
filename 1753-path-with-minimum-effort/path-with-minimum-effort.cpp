258f405e1979a7371c63ac22027e1d64e974dd01
Ankita Mukherjee <161379822+ankita-0906@users.noreply.github.com>
2025-05-25 18:25:39 +0530

    Time: 45 ms (81.56%) | Memory: 24.5 MB (77.22%) - LeetSync

---
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int minimumEffortPath(vector<vector<int>>& heights) {
+        int n=heights.size(),m=heights[0].size();
+        vector<vector<int>>vis(n,vector<int>(m,INT_MAX));
+
+        priority_queue<pair<int,pair<int,int>>, vector<pair<int,pair<int,int>>>,
+        greater<pair<int,pair<int,int>>>>pq;
+
+        pq.push({0,{0,0}});
+        while(!pq.empty()){
+            auto [dist, coord] = pq.top();
+            int row = coord.first, col = coord.second;
+            pq.pop();
+             int drow[]={-1,0,1,0};
+             int dcol[]={0,-1,0,1};
+            if(row==n-1 && col ==m-1) return dist;
+             for(int i=0;i<4;i++){
+                int newrow=row+drow[i];
+                int newcol=col+dcol[i];
+                
+                if(newrow>=0 && newcol>=0 && newrow<n && newcol<m){
+                int newdist=max(dist,abs(heights[row][col]-heights[newrow][newcol]));
+                if(newdist<vis[newrow][newcol]){
+                    vis[newrow][newcol]=newdist;
+                    pq.push({newdist,{newrow,newcol}});
+                }
+                }
+             }
+        }
+        return vis[n-1][m-1];
+    }
+};
