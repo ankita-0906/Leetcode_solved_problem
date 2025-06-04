cdc93befe613833c6ff6724296dcf0e8a9360123
Ankita Mukherjee <161379822+ankita-0906@users.noreply.github.com>
2025-06-05 00:13:11 +0530

    Time: 0 ms (100.00%) | Memory: 118.1 MB (60.19%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -11,25 +11,32 @@
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
-        ListNode*slow=head,*fast=head;
+
+        if(head==NULL || head->next==NULL) return true;
+        ListNode*slow=head,*fast=head->next;
 
         //finding the middle 1
-        while(fast->next!=NULL && fast->next->next!=NULL){
+        while(fast!=NULL && fast->next!=NULL){
             slow=slow->next; fast=fast->next->next;
         }
 
-        // reverse the LL from next to middle 1 to end
+       ListNode*head2=slow->next;
+       slow->next=NULL;
+       ListNode*pre2=head2,*fut2=head2,*prev2=NULL;
+      
+     //reverse a linked list
+    while(pre2){
+      fut2=fut2->next;
+      pre2->next=prev2; prev2=pre2;pre2=fut2;
+      }
+      head2=prev2;
+       ListNode*temp1=head,*temp2=head2;
+      while(temp1 && temp2){
+        if(temp1->val!=temp2->val) return false;
+        temp1=temp1->next;
+        temp2=temp2->next;
+      }
+      return true;
 
-        ListNode*prev=NULL,*pre=slow->next,*fut=slow->next;
-        while(pre){
-            fut=fut->next;
-            pre->next=prev; prev=pre; pre=fut;
-        }
-        slow=head;
-        while(prev){
-            if(slow->val!=prev->val) return false;
-            slow=slow->next;prev=prev->next;
-        }
-        return true;
     }
 };
