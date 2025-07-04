a6d6f7f44102d856b4d6efd56954cae980cb2ae8
Ankita Mukherjee <161379822+ankita-0906@users.noreply.github.com>
2025-07-04 17:33:44 +0530

    Time: 0 ms (100.00%) | Memory: 20.4 MB (62.43%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,24 +1,17 @@
 class Solution {
 public:
-int solve(int index, vector<int>& arr, vector<int>& dp) {
-    int n = arr.size();
-    if (index >= n - 1) return 0;
-    if (dp[index] != -1) return dp[index];
-
-    int mini = INT_MAX;
-    for (int i = index+1; i <= index+arr[index]; i++) { 
-       
-            int count =  solve( i, arr, dp);
-            if (count != INT_MAX) {
-                mini = min(mini, count + 1);
-            }
-         
-    }
-    return dp[index] = mini;
-}
     int jump(vector<int>& nums) {
-        int n=nums.size();
-        vector<int>dp(n,-1);
-        return solve(0,nums,dp);
+        int n=nums.size();int right=INT_MIN;
+        int step=0,l=0,r=0;
+        while(r<n-1){
+         
+         for(int i=l;i<=r;i++){
+            right=max(i+nums[i],right);
+         }
+          l=r+1,r=right;
+         
+         step++;
+        }
+        return step;
     }
 };
