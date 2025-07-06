a999acdee35e01e1038fc78a6124e44e94410235
Ankita Mukherjee <161379822+ankita-0906@users.noreply.github.com>
2025-07-07 01:29:02 +0530

    Time: 13 ms (90.62%) | Memory: 81.1 MB (86.25%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -3,8 +3,8 @@ public:
     int largestRectangleArea(vector<int>& arr) {
         int n=arr.size();
         int i=0,prev_small,next_small,width,maxarea=0; stack<int>st;
-        while(i<=n){
-          while(!st.empty() && (i==n ||arr[st.top()]>arr[i])){
+        while(i<n){
+          while(!st.empty() && arr[st.top()]>arr[i]){
             int ele=arr[st.top()]; next_small=i;
             st.pop(); 
             if(st.empty())  prev_small=-1;
@@ -15,6 +15,15 @@ public:
           st.push(i);
           i++;
         }
+        next_small = n;
+        while (!st.empty()) {
+            int ele = st.top(); st.pop();
+            if (st.empty()) prev_small = -1;
+            else prev_small = st.top();
+            width = next_small - prev_small - 1;
+            maxarea = max(maxarea, width * arr[ele]); 
+        }
+
         return maxarea;
     }
 };
