abdb4ad1be2445b2f65b8a1dfdead9f1e226f0c8
Ankita Mukherjee <161379822+ankita-0906@users.noreply.github.com>
2025-07-13 12:48:54 +0530

    Time: 351 ms (52.25%) | Memory: 11.9 MB (12.85%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,36 +1,36 @@
 class Solution {
 public:
-
-int solve(vector<vector<char>>& board,int row,int col,string word,int index,int t_row,int t_col){
-    //base case
-    if(index==word.length()) return 1;
-    if(row<0 || row >=t_row || col<0 || col>=t_col || board[row][col]!=word[index] ||
-      board[row][col]=='&')
-      return 0;
-
-     char temp=board[row][col];
-     board[row][col]='&';
-
-     //checking upper letter
-    int ans= solve(board,row-1,col,word,index+1,t_row,t_col) || 
-             solve(board,row+1,col,word,index+1,t_row,t_col)||
-             solve(board,row,col-1,word,index+1,t_row,t_col) ||
-             solve(board,row,col+1,word,index+1,t_row,t_col);
-       
-       board[row][col]=temp;
-       return ans;
-      }
+   bool dfs(int i,int j,string word,vector<vector<char>>& board,int index,
+   vector<vector<int>>&vis,int n,int m){
     
+        if(index>=word.size()) return true;
+        int dr[]={-1,0,1,0};
+        int dc[]={0,-1,0,1};
 
+        for(int k=0;k<4;k++){
+            int nc=j+dr[k];
+            int nr=i+dc[k];
+            if(nc>=0 && nr>=0 && nc<m && nr<n && vis[nr][nc]==0 &&
+             board[nr][nc]==word[index] ){
+                vis[nr][nc]=1;
+                if(dfs(nr,nc,word,board,index+1,vis,n,m)) return true;
+                vis[nr][nc]=0;
+             }
+        }
+        return false;
+   }
     bool exist(vector<vector<char>>& board, string word) {
-        int n=word.length(),ans;
-        int t_row=board.size();
-        int t_col=board[0].size();
-        for(int i=0;i<t_row;i++){
-            for(int j=0;j<t_col;j++){
-                
-                 ans=solve(board,i,j,word,0,t_row,t_col);
-                 if(ans) return true;
+       
+        int n=board.size();
+        int m=board[0].size();
+         vector<vector<int>>vis(n,vector<int>(m,0));
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+               if(board[i][j]==word[0]) {
+                vis[i][j]=1;
+                if(dfs(i,j,word,board,1,vis,n,m)) return true;;
+                vis[i][j]=0;
+               }
             }
         }
         return false;
