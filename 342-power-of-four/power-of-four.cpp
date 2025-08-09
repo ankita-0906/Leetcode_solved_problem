f47372d888507fbeec7ad8c76e58c7a190cc8559
Ankita Mukherjee <161379822+ankita-0906@users.noreply.github.com>
2025-08-09 10:49:11 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (82.25%) - LeetSync

--- 342-power-of-four/power-of-four.cpp
+++ 342-power-of-four/power-of-four.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     bool isPowerOfFour(int n) {
-        if(n<=0) return false;
-        while(n%4==0) n=n/4;
-        return n==1;
+        // 0x55555555=01010101....0101
+        // 4-- 100
+       return (n > 0) && ((n & (n - 1)) == 0) && ((n & 0x55555555) != 0);
     }
 };
