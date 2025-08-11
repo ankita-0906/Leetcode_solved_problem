3fd176e3a23a671ec89736c731f4ee8f4d3d9c58
Ankita Mukherjee <161379822+ankita-0906@users.noreply.github.com>
2025-08-11 23:00:05 +0530

    Time: 5 ms (6.65%) | Memory: 9.2 MB (13.07%) - LeetSync

--- 768-partition-labels/partition-labels.cpp
+++ 768-partition-labels/partition-labels.cpp
@@ -16,22 +16,23 @@ public:
             auto index=it.second;
             arr.push_back({index.first,index.second});
         }
-        sort(arr.begin(), arr.end());
-
-        // Step 4: Merge intervals & get partition sizes
-        vector<int> ans;
-        int start = arr[0].first, end = arr[0].second;
-        
-        for (int i = 1; i < arr.size(); i++) {
-            if (arr[i].first <= end) { // overlap
-                end = max(end, arr[i].second);
-            } else { // no overlap → push result
-                ans.push_back(end - start + 1);
-                start = arr[i].first;
-                end = arr[i].second;
+        vector<int>ans;
+        sort(arr.begin(),arr.end());
+        int start=arr[0].first,end=arr[0].second;
+        ans.push_back(end-start+1);
+        for(int i=1;i<arr.size();i++){
+             if (arr[i].first <= end) {
+              if (arr[i].second > end) {
+              int len = ans.back();
+              ans.back() = arr[i].second - end + len; 
+              end = arr[i].second;
             }
         }
-        ans.push_back(end - start + 1);
+        else {
+         ans.push_back(arr[i].second - arr[i].first + 1);
+         end=arr[i].second ;
+        }
+        }
         return ans;
     }
 };
