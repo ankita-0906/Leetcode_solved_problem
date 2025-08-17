e7bebfc7fd6d266f17306dddd3e419d838532b8e
Ankita Mukherjee <161379822+ankita-0906@users.noreply.github.com>
2025-08-17 15:59:05 +0530

    Time: 8 ms (47.42%) | Memory: 15.7 MB (46.46%) - LeetSync

--- 1648-minimum-insertions-to-balance-a-parentheses-string/minimum-insertions-to-balance-a-parentheses-string.cpp
+++ 1648-minimum-insertions-to-balance-a-parentheses-string/minimum-insertions-to-balance-a-parentheses-string.cpp
@@ -1,30 +1,56 @@
 class Solution {
 public:
     int minInsertions(string s) {
-        int n=s.length();
-        int i=0,cnt=0;
-        stack<char>st;
-        while(i<n){
-          if(s[i]=='(') st.push(s[i]);
-          else{
-            // double ) ----->))
-            if(i!=n-1 && s[i+1]==')'){
-                if(st.empty()) cnt++;
-                else st.pop();
-                i++;
-            }
-            // single )
-            else{
-               if(st.empty()) cnt=cnt+2;   //)(
-                 else{
-                    st.pop(); cnt++;
-                }    
+        // int n=s.length();
+        // int i=0,cnt=0;
+        // stack<char>st;
+        // while(i<n){
+        //   if(s[i]=='(') st.push(s[i]);
+        //   else{
+        //     // double ) ----->))
+        //     if(i!=n-1 && s[i+1]==')'){
+        //         if(st.empty()) cnt++;
+        //         else st.pop();
+        //         i++;
+        //     }
+        //     // single )
+        //     else{
+        //        if(st.empty()) cnt=cnt+2;   //)(
+        //          else{
+        //             st.pop(); cnt++;
+        //         }    
                 
+        //     }
+        //   }
+        //   i++;
+        // }
+        // cnt=cnt+2*st.size();
+        // return cnt;
+
+        int insertions = 0; // total insertions we need to make
+        int need = 0;       // how many ')' we still need
+
+        for (char c : s) {
+            if (c == '(') {
+                // Each '(' needs 2 ')'
+                need += 2;
+
+                // If need is odd, insert one ')' now to make it even
+                if (need % 2) {
+                    insertions++; // inserted one ')'
+                    need--;       // now need is even
+                }
+            } else { // c == ')'
+                need--;
+
+                if (need < 0) {
+                    // We have more ')' than needed
+                    insertions++; // insert one '('
+                    need = 1;     // one ')' is still needed
+                }
             }
-          }
-          i++;
         }
-        cnt=cnt+2*st.size();
-        return cnt;
+
+        return insertions + need;
     }
 };
