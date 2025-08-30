31538f0301c5751064b0ef28ad32a5430279be62
Ankita Mukherjee <161379822+ankita-0906@users.noreply.github.com>
2025-08-30 13:49:22 +0530

    Time: 10 ms (51.28%) | Memory: 45.4 MB (15.86%) - LeetSync

--- 3748-sort-matrix-by-diagonals/sort-matrix-by-diagonals.cpp
+++ 3748-sort-matrix-by-diagonals/sort-matrix-by-diagonals.cpp
@@ -1,36 +1,64 @@
 class Solution {
 public:
     vector<vector<int>> sortMatrix(vector<vector<int>>& grid) {
-        int n=grid.size();
-        unordered_map<int,vector<int>>mp;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-             mp[i-j].push_back(grid[i][j]);
-             }
-        }
-             for(auto &it:mp){
-                if(it.first>=0) 
-                sort(it.second.begin(),it.second.end(),greater<int>());
-                else 
-                sort(it.second.begin(),it.second.end());
-             }
-             unordered_map<int,queue<int>>pq;
-             for(auto it:mp){
-                queue<int>q;
-                for(auto val:it.second)
-                q.push(val);
+        // int n=grid.size();
+        // unordered_map<int,vector<int>>mp;
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<n;j++){
+        //      mp[i-j].push_back(grid[i][j]);
+        //      }
+        // }
+        //      for(auto &it:mp){   // by reference otherwise it only sort the copy of the vector
+        //         if(it.first>=0) 
+        //         sort(it.second.begin(),it.second.end(),greater<int>());
+        //         else 
+        //         sort(it.second.begin(),it.second.end());
+        //      }
+        //      unordered_map<int,queue<int>>pq;
+        //      for(auto it:mp){
+        //         queue<int>q;
+        //         for(auto val:it.second)
+        //         q.push(val);
 
-                pq[it.first]=q;
-             }
+        //         pq[it.first]=q;
+        //      }
 
-             for(int i=0;i<n;i++){
-                for(int j=0;j<n;j++){
-                    grid[i][j]=pq[i-j].front();
-                    pq[i-j].pop();
-                }
-             }
+        //      for(int i=0;i<n;i++){
+        //         for(int j=0;j<n;j++){
+        //             grid[i][j]=pq[i-j].front();
+        //             pq[i-j].pop();
+        //         }
+        //      }
              
         
+        // return grid;
+
+
+        int n = grid.size(), m = grid[0].size();
+        unordered_map<int, priority_queue<int>> maxHeaps;
+        unordered_map<int, priority_queue<int, vector<int>, greater<int>>> minHeaps;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                int key = i - j;
+                if (key < 0) minHeaps[key].push(grid[i][j]);
+                else maxHeaps[key].push(grid[i][j]);
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                int key = i - j;
+                if (key < 0) {
+                    grid[i][j] = minHeaps[key].top();
+                    minHeaps[key].pop();
+                } else {
+                    grid[i][j] = maxHeaps[key].top();
+                    maxHeaps[key].pop();
+                }
+            }
+        }
         return grid;
     }
+    
 };
