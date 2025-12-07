fcc313362375f2ca0e9a8db5e14ed5fcdc748550
Ankita Mukherjee <161379822+ankita-0906@users.noreply.github.com>
2025-12-07 17:40:04 +0530

    Time: 19 ms (28.92%) | Memory: 14.2 MB (28.75%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -3,13 +3,14 @@ public:
     int lengthOfLongestSubstring(string s) {
         int n =s.length(); int maxi=0;
         int i=0,j=0,len=0; 
-        unordered_map<char,int>mpp;
-        while(i<n &&j<n){
-          mpp[s[j]]++;
-          while(mpp[s[j]]>1 && i<j){
-            mpp[s[i]]--;
+        unordered_map<char,int>mp;
+        while(j<n){
+         ;
+          while(mp.find(s[j])!=mp.end() && i<j){
+           mp.erase(s[i]);
             i++;
           }
+          mp[s[j]]++;
           len=j-i+1;
           maxi=max(len,maxi);
           j++;
