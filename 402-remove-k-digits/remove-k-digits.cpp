ffded6889e25fd1e2c43b15f6cf08ae02eab2704
Ankita Mukherjee <161379822+ankita-0906@users.noreply.github.com>
2025-12-14 13:38:09 +0530

    Time: 7 ms (24.49%) | Memory: 11.8 MB (16.95%) - LeetSync

--- 402-remove-k-digits/remove-k-digits.cpp
+++ 402-remove-k-digits/remove-k-digits.cpp
@@ -1,35 +1,27 @@
 class Solution {
 public:
     string removeKdigits(string num, int k) {
+        int n=num.length();
         stack<char>st;
-        int n=num.length(),cnt=0;
-        int i=0;
-        while(i<n){
-         while(!st.empty() && num[i]<st.top() && k>0){
-            st.pop(); k--;
-         }
-         st.push(num[i]);
-         i++;
-        }
-      while ( k>0) {
-        if(st.empty()) return "0";
-        st.pop();
-        k--;
-    }
-    string s;
-     while(!st.empty()){
-        char ele=st.top();
-         s.push_back(ele); st.pop();
-        }
-    reverse(s.begin(),s.end());
-    
-    int start = 0;
-    while (start < s.length() && s[start] == '0') {
-        start++;
-    }
-    s = s.substr(start);
+        for(int i=0;i<n;i++){
+            while(k>0 && !st.empty() && st.top()>num[i]){
+                st.pop(); k--;
+            }
+        st.push(num[i]);
 
-    // Return "0" if the string is empty
-    return s.empty() ? "0" : s;
+        }
+        while(k>0 && !st.empty()){
+          st.pop();k--;
+        } 
+        string ans="";
+        while(!st.empty()){
+          ans.push_back(st.top()); st.pop();
+        }
+        reverse(ans.begin(),ans.end());
+        int i = 0;
+        while (i < ans.length() && ans[i] == '0') {
+        i++;
+        }
+       return (i == ans.length()) ? "0" : ans.substr(i);
     }
 };
