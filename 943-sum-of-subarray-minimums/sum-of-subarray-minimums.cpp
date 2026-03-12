eeda4bd35f482b920b5e9b9b6529a4ba4153f47e
Ankita Mukherjee <161379822+ankita-0906@users.noreply.github.com>
2026-03-12 19:09:51 +0530

    Time: 8 ms (94.22%) | Memory: 47.1 MB (58.70%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
@@ -1,36 +1,43 @@
 class Solution {
 public:
     int sumSubarrayMins(vector<int>& arr) {
-       
-     int n = arr.size();
-    const int mod = 1e9 + 7;
-   int i=0,prev_small,next_small,left,right; stack<int>st;
-   long long subarray=0;
+        int n=arr.size();
+        vector<int>pre(n),suf(n);
+        pre[0]=-1;
+        stack<int>st;
+        st.push(0); int i=0;
         while(i<n){
-          while(!st.empty() && arr[st.top()]>arr[i]){
-            int index=st.top(); next_small=i;
-            st.pop(); 
-            if(st.empty())  prev_small=-1;
-             else prev_small= st.top();
-            left=index-prev_small;right=next_small-index;
-            long long ans=(((left*right*1LL)%mod)*arr[index]*1LL)%mod;
-            subarray=(subarray+ans)%mod;
-          }
-          st.push(i);
-          i++;
-        }
-        next_small = n;
-        while (!st.empty()) {
-            int index = st.top(); st.pop();
-            if (st.empty()) prev_small = -1;
-            else prev_small = st.top();
-            left=index-prev_small;right=next_small-index;
-            long long ans=(((left*right*1LL)%mod)*arr[index]*1LL)%mod;
-            subarray=(subarray+ans)%mod;
-            
-        }
-    
-        return subarray;
+            while(!st.empty() && arr[st.top()]>=arr[i]) st.pop();
 
+            if(st.empty()) pre[i]=-1;
+            else pre[i]=st.top();
+            
+            st.push(i);
+            i++;
+
+        }
+
+        while(!st.empty()) st.pop();
+
+        st.push(n-1); suf[n-1]=n;
+       i=n-2;
+       while(i>=0){
+            while(!st.empty() && arr[st.top()]>arr[i]) st.pop();
+
+            if(st.empty()) suf[i]=n;
+            else suf[i]=st.top();
+            
+            st.push(i);
+            i--;
+
+        }
+        long long sum = 0;
+        int mod = 1e9 + 7;
+        for( i=0;i<n;i++){
+          int left=i-pre[i];
+          int right=suf[i]-i;
+         sum = (sum + (1LL * left * right * arr[i]) % mod) % mod;
+        }
+        return sum;
     }
 };
