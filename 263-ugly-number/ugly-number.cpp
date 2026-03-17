588140cb26ff74ce16e568394a2db1e1e6a38d93
Ankita Mukherjee <161379822+ankita-0906@users.noreply.github.com>
2026-03-17 12:44:20 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (75.51%) - LeetSync

---
+++ 263-ugly-number/ugly-number.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool isUgly(int n) {
+        if(n<=0) return false;
+        for(int i:{2,3,5}){
+            while(n%i==0) n=n/i;
+        }
+        if(n==1 ) return true;
+        return false;
+    }
+};
