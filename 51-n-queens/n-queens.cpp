ddc3bc44c0e3f3ba1adc24c415cd7b9536a94b3e
Ankita Mukherjee <161379822+ankita-0906@users.noreply.github.com>
2026-03-22 02:02:05 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (55.17%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -1,43 +1,37 @@
 class Solution {
 public:
-  bool issafe(int row,int col,int n,vector<string> &board){
-
-    //checking each column of the row
-
-     for(int i=row-1;i>=0;i--){
-      if(board[i][col]=='Q') return false;
-     }
-
-     //checking upper left diagonal
-
-     for(int i=row-1,j=col-1;i>=0 && j>=0;i--,j--){
-        if(board[i][j]=='Q') return false;
-     }
-
-     //checking upper right diagonal
-       for(int i=row-1,j=col+1;i>=0 && j<n;i--,j++){
-        if(board[i][j]=='Q') return false;
-     }
-     return true;
+  bool ispossible(int row,int col,vector<string>&board,int n){
+    // col chceking 
+    for(int i=0;i<row;i++){
+        if(board[i][col]=='Q') return false;
+    }
+    //left diagonal 
+   for(int i=row-1,j=col-1;i>=0 && j>=0;i--,j--){
+    if(board[i][j]=='Q') return false;
+   }
+    //right diagonal 
+    for(int i=row-1,j=col+1;i>=0 && j<n;i--,j++)
+    if(board[i][j]=='Q') return false;
+    return true;
   }
-    void solve(int row,int n,vector<vector<string>>&ans,vector<string>&board){
-        if(row==n){
-         ans.push_back(board);
-         return;
+    void rec(int level, vector<vector<string>>&ans,vector<string>&board,int n){
+        if(level==n) {
+            ans.push_back(board);
+            return ;
         }
         for(int i=0;i<n;i++){
-            if(issafe(row,i,n,board)){
-              board[row][i]='Q';
-              solve(row+1,n,ans,board);
-              board[row][i]='.';
-            }
+           if(ispossible(level,i,board,n)){
+            board[level][i]='Q';
+            rec(level+1,ans,board,n);
+             board[level][i]='.';
+           }
         }
+        return;
     }
     vector<vector<string>> solveNQueens(int n) {
-        vector<string>board(n,string(n,'.'));
         vector<vector<string>>ans;
-       
-        solve(0,n,ans,board);
+        vector<string>board(n,string(n,'.'));
+        rec(0,ans,board,n);
         return ans;
     }
 };
