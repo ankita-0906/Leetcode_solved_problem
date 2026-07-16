e00e348d9818fbe2e0d62a9a132d802259ba827c
Ankita Mukherjee <161379822+ankita-0906@users.noreply.github.com>
2026-07-17 00:25:27 +0530

    Time: 9 ms (5.78%) | Memory: 22.2 MB (5.62%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,33 +1,34 @@
 class Solution {
 public:
-    void bfs(int i,vector<int>&vis,vector<vector<int>>adj){
-       queue<int>q;
-       q.push(i);
-       while(!q.empty()){
-        int node=q.front();
-        q.pop();
-        for(auto it:adj[node]){
-            if(vis[it]!=1)
-            q.push(it); vis[it]=1;
-        }
-       }
-    }
-    int findCircleNum(vector<vector<int>>& isConnected) {
-        int n=isConnected.size();
-        vector<vector<int>>adj(n);
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(isConnected[i][j]==1 && i!=j){
-                   adj[i].push_back(j);
-                  
+    void bfs(queue<int>&pq,int i,vector<int>&vis,int n,vector<vector<int>>adj){
+        while(!pq.empty()){
+            int node=pq.front(); pq.pop();
+            for(auto it:adj[node]){
+                if(vis[it]==0){
+                     pq.push(it);
+                     vis[it]=1;
                 }
             }
         }
-        vector<int>vis(n,0);int cnt=0;
+    }
+    int findCircleNum(vector<vector<int>>& arr) {
+        int n =arr[0].size();
+        vector<vector<int>>adj(n);
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i!=j && arr[i][j]==1) adj[i].push_back(j);
+            }
+        }
+
+        vector<int>vis(n,0); queue<int>pq;
+        int cnt=0;
         for(int i=0;i<n;i++){
             if(vis[i]==0){
-                cnt++; bfs(i,vis,adj);
+                pq.push(i);
                 vis[i]=1;
+                cnt++;
+                bfs(pq,i,vis,n,adj);
             }
         }
         return cnt;
